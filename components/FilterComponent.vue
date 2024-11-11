3bfb8d19aafca145c84b61dca60adc0f5d6828de
Alaaeldeen Hamed <mregydev@gmail.com>
2024-11-11 14:41:30 +0100

    fix:update way we handle filter

--- components/FilterComponent.vue
+++ components/FilterComponent.vue
@@ -1,14 +1,14 @@
 <template>
-  <VAutocomplete :items="categoryItems" item-text="title" item-value="id" class="mx-auto" density="comfortable" menu-icon=""
+  <VAutocomplete v-model="selectedItem"   :items="categoryItems" item-text="title" item-value="id" class="mx-auto" density="comfortable" menu-icon=""
     placeholder="Search by Category.." prepend-inner-icon="mdi-magnify"  theme="light"
-    variant="solo" hide-no-data auto-select-first no-data-text=false  @keyup="onKeyUp"
-    @update:model-value="onItemClick"></VAutocomplete>
+    variant="solo" hide-no-data auto-select-first no-data-text=false @update:model-value="onItemClick"  @keyup="onKeyUp"
+    @keyup.enter="onItemClick" @change="onItemClick"></VAutocomplete>
 </template>
 <script lang="ts" setup>
 
 import { useProductStore } from '~/store/productStore';
 
-
+const selectedItem=ref()
 const store = useProductStore();
 const { fetchProductCategories } = store
 const { categories } = storeToRefs(store)
@@ -24,10 +24,13 @@ const { debounceFn: onKeyUp } = useDebounce(
 
 
 const categoryItems = computed(() => categories.value?.map(category => ({ title: category.name , id:category.id})))
-
-const onItemClick = (value: string) => {
-  if (value) {
-    router.push(`/?category=${value}`)
+watch(router.currentRoute.value,()=>{
+  selectedItem.value=""
+})
+const onItemClick = () => {
+  const id =selectedItem.value
+  if (id) {
+    router.push(`/?category=${id}`)
   }
   else
   router.push(`/`)
